174cb97025aaf29dc7b796bf09903c5618d86283
DanielPra <59303233+DanielPra@users.noreply.github.com>
2020-04-01 10:07:05 +0200

    Create e20.py

---
+++ e20.py
@@ -0,0 +1,26 @@
+number = [99]
+
+list = [3, 4, 5, 6, 7, 9, 99, 111]
+
+#list = ["3", "4", "5", "6", "14", "44", "77", "88", "99" ]
+
+
+length = (len(list))
+
+middle_value = length / 2
+
+#print(middle_value)
+
+print(list(middle_value))
+
+
+"""
+def basic_search():
+    for element in list:
+        if element in number:
+            print("I found the number")
+            
+if number in list:
+    print("Correct")
+else:
+    print("Incorrect")"""

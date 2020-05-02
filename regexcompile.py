b40d695c842d8a886603ff51deb8a44ef8352c3b
DanielPra <59303233+DanielPra@users.noreply.github.com>
2020-05-02 12:06:54 +0200

    Create regexcompile.py

---
+++ regexcompile.py
@@ -0,0 +1,26 @@
+# Är rad nummer 13 nödvändig? Jag antar det men om jag stoppar in regexen i rad 16 så funkar den ändå. 
+# Dvs jag kommenterar ut rad 13 och skriver på rad 16 istället "mat = re.search(reg, passwd)"
+# Inte min kod utan hittade den på https://www.geeksforgeeks.org/password-validation-in-python/
+
+# importing re library 
+import re 
+  
+def main(): 
+    passwd = 'Geek12@'
+    reg = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$"
+      
+    # compiling regex 
+    pat = re.compile(reg) 
+      
+    # searching regex                  
+    mat = re.search(pat, passwd) 
+      
+    # validating conditions 
+    if mat: 
+        print("Password is valid.") 
+    else: 
+        print("Password invalid !!") 
+  
+# Driver Code      
+if __name__ == '__main__': 
+    main() 

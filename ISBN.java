f95e02020dbdbd23cb8e886d530ab6145c812923
DanielPra <59303233+DanielPra@users.noreply.github.com>
2020-06-18 10:58:08 +0200

    Create ISBN.java

---
+++ ISBN.java
@@ -0,0 +1,14 @@
+package db222ta_assign1;
+import java.util.Scanner;
+
+public class ISBN {
+
+  	public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      System.out.print("Enter a nine digit number: ");
+      int num;
+      num = scanner.nextInt();
+
+	}
+
+}

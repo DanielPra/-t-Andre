8e95f23513da17ad0132cc73378f020e26b0875c
DanielPra <59303233+DanielPra@users.noreply.github.com>
2020-07-08 14:38:36 +0200

    Create Testv1.java

---
+++ Testv1.java
@@ -0,0 +1,19 @@
+public class Test {
+
+   public static void main(String args[]) {
+      int x = 20;
+      int y = 15;
+
+      if( x == 30 ) {
+         if( y == 10 ) {
+            System.out.print("X = 30 and Y = 10");
+         }
+         
+      if ( x == 20 ) {
+        if( y == 15 ) {
+                System.out.print("Second if");
+             }
+      }
+   }
+}
+}
